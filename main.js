08d465d77dc42659afe3cd7b1a75df3418f29a0c
pnichols789 <pnichols789@gmail.com>
2022-10-27 13:14:34 -0500

    -

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-var cvs = document.getElementById("canvas");
-var ctx = cvs.getContext("2d");
-var reloadNeeded = false
+let cvs = document.getElementById("canvas");
+let ctx = cvs.getContext("2d");
+let reloadNeeded = false;
 // load images
 
-var bird = new Image();
-var bg = new Image();
-var fg = new Image();
-var pipeNorth = new Image();
-var pipeSouth = new Image();
+let bird = new Image();
+let bg = new Image();
+let fg = new Image();
+let pipeNorth = new Image();
+let pipeSouth = new Image();
 
 bird.src = "unicorn.png";
 bg.src = "bg.png";
@@ -15,23 +15,23 @@ fg.src = "fg.png";
 pipeNorth.src = "lollipopNorth.png";
 pipeSouth.src = "lollipopSouth.png";
 
-
+             
 // some variables
 
-var gap = 130;
-var constant;
+let gap = 130;
+let constant;
 
-var bX = 10;
-var bY = 150;
+let bX = 10;
+let bY = 150;
 
-var gravity = 1.0;
+let gravity = 1.0;
 
-var score = 0;
+let score = 0;
 
 // audio files
 
-var fly = new Audio();
-var scor = new Audio();
+let fly = new Audio();
+let scor = new Audio();
 
 fly.src = "sounds_fly.mp3";
 scor.src = "sounds_score.mp3";
@@ -47,25 +47,23 @@ function moveUp(){
 
 // pipe coordinates
 
-var pipe = [];
+let pipe = [];
 
 pipe[0] = {
     x : cvs.width,
     y : 0
 };
 
+
 // draw images
 
 function draw(){
-    if (!reloadNeeded) {
-        reloadNeeded = true;
-        window.location.reload(); // reload the page
-      }
+    
     
     ctx.drawImage(bg,0,0);
     
     
-    for(var i = 0; i < pipe.length; i++){
+    for(let i = 0; i < pipe.length; i++){
         
         constant = pipeNorth.height+gap;
         ctx.drawImage(pipeNorth,pipe[i].x,pipe[i].y);
@@ -73,6 +71,8 @@ function draw(){
              
         pipe[i].x--;
         
+  
+
         if( pipe[i].x == 125 ){
             pipe.push({
                 x : cvs.width,
@@ -83,13 +83,19 @@ function draw(){
         // detect collision
         
         if( bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || bY+bird.height >= pipe[i].y+constant) || bY + bird.height >=  cvs.height - fg.height){
-            location.reload(); // reload the page
+            if (!reloadNeeded) {
+                reloadNeeded = true;
+                window.location.reload(); // reload the page
+                }
         }
         
         if(pipe[i].x == 5){
             score++;
             scor.play();
         }
+
+
+ 
         
         
     }
@@ -100,12 +106,14 @@ function draw(){
     
     bY += gravity;
     
-    ctx.fillStyle = "#000";
+    ctx.fillStyle = "#fff";
     ctx.font = "20px Verdana";
     ctx.fillText("Score : "+score,10,cvs.height-10);
     
     requestAnimationFrame(draw);
     
+    
 }
+
 
 draw();
